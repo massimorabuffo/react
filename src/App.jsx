5dc0b9c76c3509d2683892d5b55c1f4f90c3f8b3
Massimo <rabuffo.massimo@gmail.com>
2023-10-03 20:35:04 +0200

    exercise_7 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-  const sum = (a, b) => {
-    return <h2>{a + b}</h2>
-  }
-
-  return (
+function Hello() {
+  return(
     <>
-      {sum(3, 3)}
-    </>
+     <h2>Hello, world!</h2>
+     </>
   )
 }
-
-export default App
