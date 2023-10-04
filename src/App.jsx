1c591de8dada30ad04fd70e2131358481a6a07d4
Massimo <rabuffo.massimo@gmail.com>
2023-10-04 19:47:29 +0200

    exercise_25 done

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ function Welcome({ name, age }) {
     return (
         <>
             {name ? <p>Welcome, {name}!</p> : <p>Please, insert a name!</p>}
-            {(age > 18 && age < 65) && name === "John" ? <Age age={ age } /> : !age}
+            {<Age age={ age } />}
         </>
     )
 }
@@ -10,14 +10,11 @@ function Welcome({ name, age }) {
 function Age({ age }) {
     return (
         <>
-            {!!age && <p>Your age is {age}.</p>}
+            {age > 18 ? <p>Your age is {age}.</p> : <p>You are very young!</p>}
         </>
     )
 }
 
 export function App() {
-    return <Welcome name="John" age={26}/>
+    return <Welcome name="John" age={18}/>
 }
-
-// In questo caso a riga 19 ho dovuto modificare il tipo di valore passato a "name" da JSX a stringa, altrimenti la seconda condizione
-// a riga 5 risulta sempre falsy. C'è un modo per far funzione il codice anche passando un JSX ad un prop?
