77b5b8c1e44f37a657b0e89dfc2969e64ada8b7c
Massimo <rabuffo.massimo@gmail.com>
2023-10-04 16:48:21 +0200

    exercise_17 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
 function Welcome({ name }) {
-    return <p>Welcome, {name}!</p>
+    return (
+        <>
+            {name ? <p>Welcome, {name}!</p> : <p>Please, insert a name!</p>}
+        </>
+    )
 }
 
 export function App() {
-    return <Welcome name="Massimo" />
+    return <Welcome />
 }
+
+// Se non passo alcun prop a <Welcome>, in pagina vedrò renderizzata semplicemente la stringa "Hello, !".
