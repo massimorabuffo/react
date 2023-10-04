0e15dbe77ee4a44236e38dbcdf0d772288af4fee
Massimo <rabuffo.massimo@gmail.com>
2023-10-04 14:28:21 +0200

    exercise_13 done

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,10 @@ import { Hello } from "./Hello"
 export function App() {
   return(
       <div>
+        {/* Ogni volta che uso il componente <Hello/> viene renderizzato il contenuto del suo "return" */}
+
+        <Hello />
+        <Hello />
         <Hello />
       </div>
   )
