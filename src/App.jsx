2f7e781bc30ac010930b9f04fa99e404676aa39e
Massimo <rabuffo.massimo@gmail.com>
2023-10-04 17:23:25 +0200

    exercise_20 done

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,14 @@ function Welcome({ name, age }) {
     return (
         <>
             {name ? <p>Welcome, {name}!</p> : <p>Please, insert a name!</p>}
+            <Age age={ age } />
+        </>
+    )
+}
+
+function Age({ age }) {
+    return (
+        <>
             {!!age && <p>Your age is {age}.</p>}
         </>
     )
@@ -11,5 +19,3 @@ export function App() {
     return <Welcome name={<strong>Massimo</strong>} age={26}/>
 }
 
-// Se assegno ad un prop un'espressione jsx al posto di una stringa (usando le {}), questo assume le proprietà di quello specifico tag.
-// In questo caso, <strong> è un inline-tag che rende il suo contenuto in grassetto.
