ca7d59e12da99b4661fa489a74ff654793d0c29a
Massimo <rabuffo.massimo@gmail.com>
2023-10-04 17:06:48 +0200

    exercise_19 done

--- src/App.jsx
+++ src/App.jsx
@@ -8,5 +8,8 @@ function Welcome({ name, age }) {
 }
 
 export function App() {
-    return <Welcome name="Massimo" age={26}/>
+    return <Welcome name={<strong>Massimo</strong>} age={26}/>
 }
+
+// Se assegno ad un prop un'espressione jsx al posto di una stringa (usando le {}), questo assume le proprietà di quello specifico tag.
+// In questo caso, <strong> è un inline-tag che rende il suo contenuto in grassetto.
