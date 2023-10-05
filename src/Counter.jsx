d8305c173780b3638d0b63f2e4a05009ba43c1db
Massimo <rabuffo.massimo@gmail.com>
2023-10-05 15:03:01 +0200

    controllo

---
+++ src/Counter.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+export function Counter() {
+    const [counter, setCounter] = useState(0);
+    function handleIncrement() {
+        setCounter(counter + 1);
+    }
+
+    return <>
+            <h2>{counter}</h2>
+            <button onClick={handleIncrement}>Click to increment!</button>
+            </>
+}
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import Counter from './Counter.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Counter />
   </React.StrictMode>,
 )
