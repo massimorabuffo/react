b6994035cca1b153a618baadd5922862ffc5e76a
Massimo <rabuffo.massimo@gmail.com>
2023-10-05 15:52:24 +0200

    exercise_31 done

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -4,6 +4,8 @@ export function Counter() {
     const [counter, setCounter] = useState(0)
     function handleIncrement() {
         setCounter(counter + 1)
+        // Se ho capito bene la domanda, il modo corretto di incrementare "counter" è usare la funzione setCounter (invece che riassegnare un valore direttamenter a counter, del tipo "counter++").
+        // Questo perché non è possibile riassegnare valore ad una variabile dichiarata con const (riga 4), inoltre setCounter è una funzione costruita appositamente per modellare la variabile counter.
     }
 
     return <>
