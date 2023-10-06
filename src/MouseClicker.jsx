de9fc76accbf6b6f2b27686049be2912ea2e5b49
Massimo <rabuffo.massimo@gmail.com>
2023-10-06 12:28:16 +0200

    exercise_39

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -2,8 +2,16 @@ export function MouseClicker({name}) {
     function handleButtonEvent(event) {
         console.log(event.currentTarget.name)
     }
+    function handleImgClick(event) {
+        console.log(event.currentTarget.src)
+    }
     return <button name = {name} onClick={handleButtonEvent}>
-            <img width={25} height={25}/>
+            <img src={"source of the image"}width={25} height={25} onClick={handleImgClick}/>
             Click here to see "{name}" on console!
            </button>
 }
+
+// L'attributo "name" del button viene stampato in console se si clicca sull'imagine, questo perché viene triggerato l'onClick event sia
+// dell'img sia del button.Se invece a riga 3 scrivessi event.target.name, a quel punto il "name" verrebbe stampato solo
+// al click sul button, ma non al click sull' img. Questo perché l'event.target si riferisce solo all'elemento HTML su cui si fa click col
+// mouse: in questo caso, l'img.
