90fc7f5edd2d635e78678b9dad86273055497374
Massimo <rabuffo.massimo@gmail.com>
2023-10-06 11:58:40 +0200

    exercise_38 done

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -1,6 +1,9 @@
 export function MouseClicker({name}) {
     function handleButtonEvent(event) {
-        console.log(event.target.name)
+        console.log(event.currentTarget.name)
     }
-    return <button name = {name} onClick={handleButtonEvent}>Click here to see "{name}" on console!</button>
+    return <button name = {name} onClick={handleButtonEvent}>
+            <img width={25} height={25}/>
+            Click here to see "{name}" on console!
+           </button>
 }
