839992a159abc2ab6c616c09e6c0662516bf38d5
Massimo <rabuffo.massimo@gmail.com>
2023-10-09 14:51:06 +0200

    exercise_42 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react"
+
+function Welcome({name}) {
+  return <>
+          <p>Welcome, {name}!</p>
+          </>
+}
+
+function InteractiveWelcome() {
+  const [value, setValue] = useState('')
+
+  function handleChange(event) {
+    const value = event.target.value;
+    setValue(value);
+  }
+
+  return <>
+          <input value = {value} onChange={handleChange} type="text"/>
+          <Welcome name= {value}/>
+        </>
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <>
+          <InteractiveWelcome />
+         </>
 }
 
 export default App
+
+
+
