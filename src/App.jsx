2b2e79443c8a7c7d63d86f75da5be0973b4f6c4e
Massimo <rabuffo.massimo@gmail.com>
2023-10-09 15:23:55 +0200

    exercise_43 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,37 @@
 import { useState } from "react"
 
-function Welcome({name}) {
-  return <>
-          <p>Welcome, {name}!</p>
-          </>
-}
-
-function InteractiveWelcome() {
-  const [value, setValue] = useState('')
+function Login() {
+  const [data, setData] = useState({
+    userName: '',
+    password: '',
+    remember: false,
+  })
 
   function handleChange(event) {
     const value = event.target.value;
-    setValue(value);
+    const name = event.target.name;
+    const checked = event.target.checked;
+    console.log(checked)
+
+    setData((data) => {
+      return {
+        ...data,
+        [name]: name === 'remember' ? checked : value,
+      }
+    })
   }
 
   return <>
-          <input value = {value} onChange={handleChange} type="text"/>
-          <Welcome name= {value}/>
-        </>
+          <input type="text" name = 'userName' value = {data.userName} onChange={handleChange}/>
+          <input type="password" name = 'password' value = {data.password} onChange={handleChange}/>
+          <input type="checkbox" name = 'remember' checked = {data.remember} onChange={handleChange} />
+          <label>Remember me</label>
+         </>
 }
 
 function App() {
   return <>
-          <InteractiveWelcome />
+          <Login />
          </>
 }
 
