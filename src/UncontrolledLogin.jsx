e5eab14e24df7fc047c39e39073b3947681593a7
Massimo <rabuffo.massimo@gmail.com>
2023-10-10 16:49:16 +0200

    commit di prova

--- src/UncontrolledLogin.jsx
+++ src/UncontrolledLogin.jsx
@@ -2,23 +2,7 @@ const UncontrolledLogin = () => {
     const handleFormSubmit = (event) => {
         event.preventDefault();
 
-        const formData = new FormData(event.target);
-
-        const data = {
-            username: formData.get('username'),
-            password: formData.get('password'),
-            remember: formData.get('remember') === 'on' ? 'ON' : 'OFF',
-        }
-
-        // const username = event.target.elements.namedItem('username').value;
-        // const password = event.target.elements.namedItem('password').value;
-        // const remember = event.target.elements.namedItem('remember').checked
-        
-        // const data = {
-        //     username, password, remember
-        // }
-
-        console.log(data);
+     console.log(event.target.elemnts)
     }
 
     return (
