968c7d2200bf328dfdaf7d5efb156ac117a6ef9c
Massimo <rabuffo.massimo@gmail.com>
2023-10-11 18:23:40 +0200

    esercizio corretto

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,4 @@
-function Welcome({ name, age }) {
-    return (
-        <>
-            {name ? <p>Welcome, {name}!</p> : <p>Please, insert a name!</p>}
-            {!!age && <p>Your age is {age}.</p>}
-        </>
-    )
-}
+import Welcome from "./Welcome";
 
 export function App() {
     return <Welcome name="Massimo" age={26}/>
