31fbf7fc797be1f92319b7c03f709e646bc2fda6
Massimo <rabuffo.massimo@gmail.com>
2023-10-11 18:14:53 +0200

    per risolverlo ho dovuto usare il metodo concat, anche se non lo avevo mai visto

---
+++ src/ToDoList.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react"
+
+const ToDoList = ({ array }) => {
+
+    const [items, setItems] = useState(array);
+    const [data, setData] = useState('');
+
+    const handleInput = (event) => {
+        const input = event.target.value;
+        setData(input);
+    }
+
+    const handleAddString = () => {
+        console.log(data)
+        setItems(items => items.concat(data))
+    }
+
+    return(<>
+            <ul>
+                {items.map((element, index) => (
+                    <li key={index}>{element}</li>
+                ))}
+            </ul>
+            <input type="text" value = {data} onChange={handleInput}/>
+            <button onClick={handleAddString}>Push to add!</button>
+        </>)
+}
+
+export default ToDoList
