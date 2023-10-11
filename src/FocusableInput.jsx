fe01375731c74f62bd1415eb2719388a5987d7ae
Massimo <rabuffo.massimo@gmail.com>
2023-10-11 09:38:32 +0200

    exercise_52 done

--- src/FocusableInput.jsx
+++ src/FocusableInput.jsx
@@ -2,7 +2,12 @@ import { useEffect, useRef } from "react"
 
 const FocusableInput = () => {
     const inputRef = useRef(null);
+
     useEffect(() => {
+        if(!inputRef.current.value) {
+            console.log('First mount');
+            inputRef.current.value = 'Changing the value of the input for the exercise';
+        }
         inputRef.current.focus();
     }, [])
 
