5e4dff9c52571e9c2d15bd075d7ac4c116e84739
Massimo <rabuffo.massimo@gmail.com>
2023-10-12 18:02:59 +0200

    exercise_60

---
+++ src/Welcome.jsx
@@ -0,0 +1,7 @@
+const Welcome = () => {
+    return <div className="welcome">
+            <h1>Hello, world!</h1>
+            </div>
+}
+
+export default Welcome
---
+++ src/index.jsx
@@ -0,0 +1,11 @@
+import Welcome from "./Welcome"
+import './index.css'
+
+
+const Index = () => {
+  return <>
+          <Welcome className='welcome'/>
+        </>
+}
+
+export default Index
