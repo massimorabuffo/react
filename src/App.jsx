74bd4d3e0e4751b78ff65a4fe44d35ff3e9a0c8e
Massimo <rabuffo.massimo@gmail.com>
2023-10-17 12:36:06 +0200

    exercise_73 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { useState } from "react"
-import { Clock } from "./Clock"
-import { LanguageContext } from "./LanguageContext";
+import GitHubUser from "./GitHubUser";
 
 export function App() {
     const [language, setLanguage] = useState('it');
@@ -9,16 +8,7 @@ export function App() {
         setLanguage(event.target.value);
     }
 
-    // Se elimino il Context Provider, il componente non riceverà alcun "value" dal context, e quindi riceverà il valore che,
-    // nel file LanguageContext.jsx, abbiamo impostato come di default, in questo caso "it". Quindi verrà mostarta la stringa italiana
-
     return <>
-        
-                <select onChange={handleChangeLanguage}>
-                    <option value="it">ITA</option>
-                    <option value="en">ENG</option>
-                </select>
-                <Clock />
-        
+            <GitHubUser username='massimorabuffo'/>
             </>
 }
---
+++ src/GitHubUser.jsx
@@ -0,0 +1,27 @@
+import { useEffect, useState } from "react"
+
+const GitHubUser = ({ username }) => {
+    const [data, setData] = useState(null);
+
+    const fetchData = async () => {
+        try{
+            const response = await fetch(`https://api.github.com/users/${username}`);
+            const json = await response.json();
+            setData(json);
+        }catch(error){
+            console.error(error);
+        }
+    }
+
+    useEffect(() => {
+        fetchData(username);
+    }, [username])
+
+    return <>
+            <h3>{data.name}</h3>
+            <h3>{data.login}</h3>
+            <img src={data.avatar_url} alt="avatar" />
+        </>
+}
+ 
+export default GitHubUser
