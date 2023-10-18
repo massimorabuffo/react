f36beb80300801cddca1ef45abac4ad97d5d450b
Massimo <rabuffo.massimo@gmail.com>
2023-10-18 15:34:50 +0200

    exercise_77 done

--- src/GitHubUser.jsx
+++ src/GitHubUser.jsx
@@ -1,9 +1,11 @@
 import useGitHubUser from "./hookGitHubUser"
 
 const GitHubUser = ({ username }) => {
-    const {data} = useGitHubUser(username)
+    const {data, loading, error} = useGitHubUser(username)
   
     return <>
+            {loading && <h3>Loading...</h3>}
+            {error && <h3>Error!</h3>}
             <h3>{data.name}</h3>
             <h3>{data.login}</h3>
             <img src={data.avatar_url} alt="avatar" />
--- src/hookGitHubUser.jsx
+++ src/hookGitHubUser.jsx
@@ -2,14 +2,27 @@ import { useEffect, useState } from "react"
 
 const useGitHubUser = (username) => {
     const [data, setData] = useState(null);
+    const [loading, setLoading] = useState(false);
+    const [error, setError] = useState(null);
 
     const fetchData = async (username) => {
+        setLoading(true);
+        setError(null);
+
         try{
             const response = await fetch(`https://api.github.com/users/${username}`);
             const json = await response.json();
+
+            if(response.status !== 200){
+                setError(new Error());
+            }
             setData(json);
-        }catch(error){
+
+        } catch (error){
             console.error(error);
+            setData(null);
+        } finally {
+            setLoading(false);
         }
     }
 
@@ -17,7 +30,11 @@ const useGitHubUser = (username) => {
         fetchData(username);
     }, [username])
 
-    return {data}
+    return {
+        data,
+        loading,
+        error,
+    }
 }
 
 export default useGitHubUser
