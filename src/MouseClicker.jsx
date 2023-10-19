126e71353c442e93c65890573037af0dc9fcaac4
Massimo <rabuffo.massimo@gmail.com>
2023-10-19 18:02:33 +0200

    esercizio corretto

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -3,7 +3,7 @@ export function MouseClicker({name}) {
         console.log(event.currentTarget.name)
     }
     return <button name = {name} onClick={handleButtonEvent}>
-            <img width={25} height={25}/>
-            Click here to see "{name}" on console!
+                <img width={25} height={25}/>
+                Click here to see "{name}" on console!
            </button>
 }
