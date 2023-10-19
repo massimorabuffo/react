779db014fc0607c171a647c5c44f69ceeb159526
Massimo <rabuffo.massimo@gmail.com>
2023-10-19 17:22:51 +0200

    esercizio corretto

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,13 +1,14 @@
 import { useState } from 'react'
 
 export function Counter({initialValue, incrementAmount}) {
-    const [counter, setCounter] = useState(initialValue)
+    const [counter, setCounter] = useState(initialValue);
+
     function handleIncrement() {
-        setCounter(counter + incrementAmount)
+        setCounter(c => c + incrementAmount);
     }
 
     return <>
-            <h2>{counter}</h2>
-            <button onClick={handleIncrement}>Click to increment!</button>
+                <h2>{counter}</h2>
+                <button onClick={handleIncrement}>Click to increment!</button>
             </>
 }
