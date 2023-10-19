49e171ddd286acfff8061b57297e099ab883b106
Massimo <rabuffo.massimo@gmail.com>
2023-10-19 14:35:34 +0200

    esercizio corretto

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -2,7 +2,7 @@ function Welcome({ name, age }) {
     return (
         <>
             {name ? <p>Welcome, {name}!</p> : <p>Please, insert a name!</p>}
-            {!!age && <p>Your age is {age}.</p>}
+            {age && <p>Your age is {age}.</p>}
         </>
     )
 }
