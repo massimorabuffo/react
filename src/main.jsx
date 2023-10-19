562060ae0f7baeb4169621b51c1d42fe9649e6cc
Massimo <rabuffo.massimo@gmail.com>
2023-10-19 17:10:29 +0200

    esercizio corretto

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { Counter } from './Counter.jsx'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Counter />
+    <App />
   </React.StrictMode>,
 )
