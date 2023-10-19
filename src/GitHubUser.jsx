04daa536adc272c7f3a6ea7ef46c69d0281d2dc6
Massimo <rabuffo.massimo@gmail.com>
2023-10-19 14:02:28 +0200

    esercizio corretto

--- src/GitHubUser.jsx
+++ src/GitHubUser.jsx
@@ -16,11 +16,17 @@ const GitHubUser = ({ username }) => {
     useEffect(() => {
         fetchData(username);
     }, [username])
-
+    
     return <>
-            <h3>{data.name}</h3>
-            <h3>{data.login}</h3>
-            <img src={data.avatar_url} alt="avatar" />
+            {!data && <div>Loading...</div>}{
+                data && 
+                    <>
+                        <h3>{data.name}</h3>
+                        <h3>{data.login}</h3>
+                        <img src={data.avatar_url} alt="avatar" />
+                    </>
+            }
+            
         </>
 }
  
