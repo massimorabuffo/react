a04c283a55ecca4e77f4287e09c48c5ae441f164
Massimo <rabuffo.massimo@gmail.com>
2023-10-19 18:25:02 +0200

    esercizio corretto

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,4 @@
-import { useState } from "react"
-
-function Login() {
-  const [data, setData] = useState({
-    userName: '',
-    password: '',
-    remember: false,
-  })
-
-  function handleChange(event) {
-    const value = event.target.value;
-    const name = event.target.name;
-    const checked = event.target.checked;
-    console.log(checked)
-
-    setData((data) => {
-      return {
-        ...data,
-        [name]: name === 'remember' ? checked : value,
-      }
-    })
-  }
-
-  return <>
-          <input type="text" name = 'userName' value = {data.userName} onChange={handleChange}/>
-          <input type="password" name = 'password' value = {data.password} onChange={handleChange}/>
-          <input type="checkbox" name = 'remember' checked = {data.remember} onChange={handleChange} />
-          <label>Remember me</label>
-         </>
-}
+import { Login } from "./Login"
 
 function App() {
   return <>
