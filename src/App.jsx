7dafd9cdd8ee9dcb1248189419f9a77aa2af3ac3
Massimo <rabuffo.massimo@gmail.com>
2023-10-23 11:56:44 +0200

    exercise_83 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import Welcome from "./Welcome"
 import './App.css'
 import { Route, Routes } from "react-router-dom"
+import Counter from "./Counter"
 
 
 const App = () => {
   return <>
           <Routes>
             <Route path="/" element={<Welcome name='world'/>}/>
+            <Route path='/counter' element={<Counter />} />
           </Routes>
         </>
 }
