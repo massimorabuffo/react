5116dd575465fb2023490bf1bbb07f3e8530ef4b
Massimo <rabuffo.massimo@gmail.com>
2023-10-26 12:12:28 +0200

    sercise_done

--- src/hookGitHubUser.jsx
+++ src/hookGitHubUser.jsx
@@ -3,7 +3,7 @@ import useSWr from "SWR"
 const fetcher = url => fetch(url).then(response => response.json());
 
 const useGitHubUser = (username) => {
-    const {data, error} = useSWr(`https://api.github.com/users/${username}`, fetcher);
+    const {data, error} = username && useSWr(`https://api.github.com/users/${username}`, fetcher);
 
     return {
         user: data,
